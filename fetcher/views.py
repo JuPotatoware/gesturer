05136e4a0a4a5e5704277d8efe60bad8a3c47b43
Matias Bryner <matib555@gmail.com>
2018-12-15 16:44:12 +0200

    hello world avia

--- fetcher/views.py
+++ fetcher/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse
 
-from django.shortcuts import render
+HttpResponse()
+
+def index(request):
+    return HttpResponse("Hello, world. How are you? Avia.")
 
 # Create your views here.
